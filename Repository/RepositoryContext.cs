a6061ec2819a1bbee501cddef0ed48bbff6caa20
softimi <softimideji@gmail.com>
2024-02-09 00:14:56 +0100

    09022024-1214

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+
+    
+}
