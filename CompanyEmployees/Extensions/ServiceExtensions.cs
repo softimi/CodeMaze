9c8bb38d61ac1b9970480877a601c6b79202f102
softimi <softimideji@gmail.com>
2024-03-04 21:51:20 +0100

    Chapter-7
    Complete version of chapter 7

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -38,6 +38,11 @@ namespace CompanyEmployees.Extensions
             services.AddDbContext<RepositoryContext>(opts =>
                 opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+        builder.AddMvcOptions(config => config.OutputFormatters.Add(new
+        CsvOutputFormatter()));
+
+
 
     }
 }
--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -15,10 +15,15 @@ namespace CompanyEmployees
         public MappingProfile()
         {
             CreateMap<Company, CompanyDto>()
+                .ForMember(c => c.FullAddress,
+                opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+            CreateMap<Employee, EmployeeDto>();
+
+            /*CreateMap<Company, CompanyDto>()
             .ForCtorParam("FullAddress",
             opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
-            CreateMap<Employee, EmployeeDto>();
+            CreateMap<Employee, EmployeeDto>();*/
         }
 
     }
