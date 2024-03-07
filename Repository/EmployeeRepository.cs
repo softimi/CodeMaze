cf0bbf4773795004fc65823acddeb8e31a0677f9
softimi <softimideji@gmail.com>
2024-03-07 14:24:40 +0100

    Chapter-9
    Complete version of chapter 9

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -24,6 +24,11 @@ namespace Repository
                 trackChanges)
                     .SingleOrDefault();
 
+        public void CreateEmployeeForCompany(Guid companyId, Employee employee)
+        {
+            employee.CompanyId = companyId;
+            Create(employee);
+        }
 
     }
 }
