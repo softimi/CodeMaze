9741b060245985369e2d89c3c658d7ab3dd7a106
softimi <softimideji@gmail.com>
2024-03-25 01:48:58 +0100

    Chapter-15
    Complete version of chapter 15

--- Shared/RequestFeatures/RequestParameters.cs
+++ Shared/RequestFeatures/RequestParameters.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Shared.RequestFeatures
 {
-    internal class RequestParameters
+    public abstract class RequestParameters
     {
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+
     }
 }
